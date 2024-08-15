a0a89ac7e086d4812495d5b8850939d44ea7c195
fakaha <lunastra34@gmail.com>
2024-08-15 23:24:08 +0700

    feat: add total price when item in cart added and push total price to localstorage

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -6,12 +6,23 @@ import { Footer } from "../components/elements/CardProduct/Footer";
 import { Button } from "../components/elements/Button";
 
 export const ProductsPage = () => {
-  const [cart, setCart] = useState([
-    {
-      id: 1,
-      qty: 1,
+  const [cart, setCart] = useState([])
+  const [totalPrice, setTotalPrice] = useState(0)
+  useEffect(() => {
+    setCart(JSON.parse(localStorage.getItem('cart')) || [])
+  }, [])
+  
+  useEffect(() => {
+    if(cart.length > 0){
+      const sum = cart.reduce((acc, item) => {
+        const product = products.find((product) => product.id === item.id)
+        return acc + product.price * item.qty
+      }, 0)
+      setTotalPrice(sum)
+      localStorage.setItem('cart', JSON.stringify(cart))
     }
-  ])
+  }, [cart])
+
 
   const handleAddToCart = (id) => {
     if(cart.find(item => item.id === id)) {
@@ -103,6 +114,14 @@ export const ProductsPage = () => {
                 </tr>
               )
             })}
+            <tr>
+              <td colSpan={3}>
+                <b>Total Price</b>
+              </td>
+              <td>
+                <b>{totalPrice.toLocaleString("id-ID", {style: 'currency', currency: 'IDR', maximumFractionDigits: 0})}</b>
+              </td>
+            </tr>
           </tbody>
         </table>
         </div>
